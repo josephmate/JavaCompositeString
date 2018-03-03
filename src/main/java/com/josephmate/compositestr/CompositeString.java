ae3016729a65b91c0d224463e4601023290fad33
Joseph Mate <mate.joseph@gmail.com>
2018-03-03 15:48:30 -0500

    Initialize the project
    added grade build build
    added empty classes

---
+++ src/main/java/com/josephmate/compositestr/CompositeString.java
@@ -0,0 +1,5 @@
+package com.josephmate.compositestr;
+
+public class CompositeString {
+
+}
---
+++ src/test/java/com/josephmate/compositestr/CompositeStringTest.java
@@ -0,0 +1,12 @@
+package com.josephmate.compositestr;
+
+import org.junit.Test;
+
+public class CompositeStringTest {
+	
+	@Test
+	public void test() {
+		
+	}
+	
+}
